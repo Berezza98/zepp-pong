fe11f8d24efb6753488592a59134143bb5f84139
dixy52-beep <71886581+dixy52-beep@users.noreply.github.com>
2022-07-04 20:29:46 +0200

    Update index.js

--- consts/index.js
+++ consts/index.js
@@ -12,3 +12,5 @@ export const LOCAL_STORAGE_KEY = 'zepp-pong.txt';
 export const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = hmSetting.getDeviceInfo();
 
 export const SCREEN_CENTER = new Vector(DEVICE_WIDTH / 2, DEVICE_HEIGHT / 2);
+
+export const deviceInfo = hmSetting.getDeviceInfo();
